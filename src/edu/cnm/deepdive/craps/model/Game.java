b57aac38f015e80a8d09b0c33faada19a89140e7
Travis Decker <decker4693@gmail.com>
2018-10-16 17:58:36 -0600

    updated controller and model

--- src/edu/cnm/deepdive/craps/model/Game.java
+++ src/edu/cnm/deepdive/craps/model/Game.java
@@ -7,16 +7,31 @@ import java.util.Random;
 
 public class Game {
 
-  private State state = State.COME_OUT;
+  private final Object lock = new Object();
+
+  private State state;
   private int point;
   private Random rng;
   private List<Roll> rolls;
+  private int wins;
+  private int losses;
 
   public Game(Random rng) {
     this.rng = rng;
     rolls = new LinkedList<>();
+    wins = 0;
+    losses = 0;
   }
-  public State roll() {
+
+  public void reset() {
+    state = State.COME_OUT;
+    point = 0;
+    synchronized (lock) {
+      rolls.clear();
+    }
+  }
+
+  private State roll() {
     int[] dice = {
         1 + rng.nextInt(6),
         1 + rng.nextInt(6)
@@ -27,13 +42,21 @@ public class Game {
       point = total;
     }
     this.state = state;
-    rolls.add(new Roll(dice, state));
+    synchronized (lock) {
+      rolls.add(new Roll(dice, state));
+    }
     return state;
   }
 
   public State play() {
+    reset();
     while (state != State.WIN && state != State.LOSS) {
       roll();
+    }
+    if (state == State.WIN) {
+      wins++;
+    } else {
+      losses++;
     }
     return state;
   }
@@ -43,30 +66,42 @@ public class Game {
   }
 
   public List<Roll> getRolls() {
-    return new LinkedList<>(rolls);
+    synchronized (lock) {
+      return new LinkedList<>(rolls);
+    }
+  }
+
+  public int getWins() {
+    return wins;
+  }
+
+  public int getLosses() {
+    return losses;
   }
 
   public static class Roll {
+
     private final int[] dice;
     private final State state;
 
-  public Roll(int[] dice, State state) {
-    this.dice = Arrays.copyOf(dice, 2);
-    this.state = state;
-  }
+    private Roll(int[] dice, State state) {
+      this.dice = Arrays.copyOf(dice, 2);
+      this.state = state;
+    }
 
-  public int[] getDice() {
-    return Arrays.copyOf(dice, 2);
-  }
+    public int[] getDice() {
+      return Arrays.copyOf(dice, 2);
+    }
 
-  public State getState() {
-    return state;
-  }
+    public State getState() {
+      return state;
+    }
 
     @Override
     public String toString() {
       return String.format("%s %s%n", Arrays.toString(dice), state);
     }
+
   }
 
   public enum State {
@@ -102,9 +137,10 @@ public class Game {
       }
     };
 
-    public  State roll(int total, int point) {
+    public State roll(int total, int point) {
       return this;
     }
 
   }
+
 }
